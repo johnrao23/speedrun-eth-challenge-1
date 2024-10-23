a4d802028462acf576659640fc239acd0de46c69
johnrao23 <86802699+johnrao23@users.noreply.github.com>
2024-10-23 12:14:27 -0400

    Update page.tsx

--- packages/nextjs/app/stakings/page.tsx
+++ packages/nextjs/app/stakings/page.tsx
@@ -9,7 +9,7 @@ const Stakings: NextPage = () => {
   const { data: stakeEvents, isLoading } = useScaffoldEventHistory({
     contractName: "Staker",
     eventName: "Stake",
-    fromBlock: 0n,
+    fromBlock: 3750241n, // Replace with your actual block number minus 10
   });
 
   if (isLoading)
