4775aea521d7a8b30f9047afeee1e2faa5d35b86
johnrao23 <86802699+johnrao23@users.noreply.github.com>
2024-10-23 13:11:11 -0400

    background colors

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex items-center flex-col flex-grow pt-10">
+    <div className="flex items-center flex-col flex-grow pt-10 light-mode-bg">
       <div className="px-5 w-[90%] md:w-[75%]">
         <h1 className="text-center mb-6">
           <span className="block text-2xl mb-2">SpeedRunEthereum</span>
